7dfd13b2158c9f7ca905aa03e042d34e85b0b278
Manish Kumar <mks893767@gmail.com>
2025-07-30 16:26:47 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -86,7 +86,7 @@ app.use('/api/stats', statsRoutes);
 // ✅ Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
   });
 }
